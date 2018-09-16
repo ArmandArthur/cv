1a8f1fa7d7de71e3c5128ed5474a5221d57b8859
ArmandArthur <armand.arthur@gmail.com>
2018-09-16 18:45:40 +0200

    table

--- get-constante.js
+++ get-constante.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 const sequelize = new Sequelize('cv', 'root', 'admin2019!!', {
-  host: 'localhost',
+  host: '127.0.0.1',
   dialect: 'mysql',
   operatorsAliases: false,	
   pool: {
