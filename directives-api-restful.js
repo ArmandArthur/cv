5ad59d80bf4fd128a5a3a0b8ed1c5849d052b7b5
ArmandArthur <armand.arthur@gmail.com>
2018-09-26 19:10:22 +0200

    mobile version

--- directives-api-restful.js
+++ directives-api-restful.js
@@ -1,7 +1,7 @@
 angular.module("DirectivesApiRestful", ["ngResource"])
 .factory("restfulService", function($resource){
 
-     var url = "http://armand-arthur.com:8085/api/"; production
+     var url = "http://armand-arthur.com:8085/api/"; 
     //var url = "http://arthur.cv-angular-node.com:8085/api/"
     var serviceRest  = $resource(
         url, 
