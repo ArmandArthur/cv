6ddf9749a820fb7bfd9a7360286a27a0292bbce9
ArmandArthur <armand.arthur@gmail.com>
2018-10-06 15:54:32 +0200

    debug*

--- app-bootstrap.js
+++ app-bootstrap.js
@@ -1,10 +1,2 @@
 
 
-$(function() {
-    $('.navbar a').on('click', function(){ 
-        if($('.navbar-toggle').css('display') !='none'){
-            $('.navbar-toggle').trigger( "click" );
-        }
-    });
-});
-
