169637f2d1d6b14fbfd43467607b79fc9b9c571c
ArmandArthur <armand.arthur@gmail.com>
2018-10-18 20:24:17 +0200

    new version

--- get-api.js
+++ get-api.js
@@ -118,7 +118,6 @@ bcrypt.genSalt(10, function(err, salt) {
 							'id': utilisateurItem.get('id')
 						}
 					}).then(utilisateur => {
-						console.info(req.session);
 						  if (!req.session.utilisateurs) {
 						    req.session.utilisateurs = {}
 						  }
