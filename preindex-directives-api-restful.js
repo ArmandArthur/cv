a993bd5c0080e3a913a515eced5c5c7316fb89eb
ArmandArthur <armand.arthur@gmail.com>
2018-10-18 13:28:21 +0200

    new version

--- preindex-directives-api-restful.js
+++ preindex-directives-api-restful.js
@@ -1,8 +1,8 @@
 angular.module("DirectivesApiRestfulPreIndex", ["ngResource"])
 .factory("preindexRestfulService", function($resource){
 
-    //var url = "http://armand-arthur.com:8085/api/"; 
-    var url = "http://arthur.cv-angular-node.com:8085/api/"
+    var url = "http://armand-arthur.com:8085/api/"; 
+    //var url = "http://arthur.cv-angular-node.com:8085/api/"
     var preindexServiceRest = $resource(url+'inscription_crud', null, {});
 
     return {
