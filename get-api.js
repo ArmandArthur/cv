0f7afadd66ed2f40a3223e6151735ce187d1a303
ArmandArthur <armand.arthur@gmail.com>
2018-10-18 22:28:22 +0200

    new version

--- get-api.js
+++ get-api.js
@@ -67,12 +67,12 @@ setTimeout(function(){ UtilisateurRequete.sync(); }, 4600);
 
 exports.verificationCaptcha = function (req, res, next) {
 	console.info(req.body);
- if(req.body['recaptcha'] === undefined || req.body['recaptcha'] === '' || req.body['recaptcha'] === null) 
+ if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) 
  {
     return res.sendStatus(404);
   }
   var secretKey = "6LfsiHUUAAAAAJgzd6dFvKfRnJvXeoWvFQBwpjlU";
-  var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['recaptcha'] + "&remoteip=" + req.connection.remoteAddress;
+  var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
   request(verificationUrl,function(error,response,body) {
     body = JSON.parse(body);
     // Success will be true or false depending upon captcha validation.
@@ -125,7 +125,7 @@ bcrypt.genSalt(10, function(err, salt) {
 
   						req.session.utilisateurs[utilisateur.get('id')] = utilisateur;
   							
-						return res.sendStatus(200);
+						return res.redirect('/index.html')
 						
 					})
 				})
