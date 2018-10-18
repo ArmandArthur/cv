3fcd02ea571b7ab0940afd5a78cddafa8da91f36
ArmandArthur <armand.arthur@gmail.com>
2018-10-18 13:27:08 +0200

    new version

--- preindex-app.js
+++ preindex-app.js
@@ -1,8 +1,8 @@
 angular.module("preindexcv", [ "ngSanitize", "preindexDirectives", "DirectivesApiRestfulPreIndex"])
-.controller("preindexController", function($scope, $location, restfulService) {
+.controller("preindexController", function($scope, $location, preindexRestfulService) {
 	$scope.$on('inscriptionFormulaireSubmit', function(event, inscription) {
 		//console.info($scope.get_categorie_structure());
-       restfulService.inscription(inscription).then(function(framework){
+       preindexRestfulService.inscription(inscription).then(function(framework){
 	  		
 	  		
 	  	})
