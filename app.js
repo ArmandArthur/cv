1ab5b424814d473b74245394db59d3b41c0e4e52
ArmandArthur <armand.arthur@gmail.com>
2018-10-19 23:18:28 +0200

    new version

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var app = angular.module("cv", [ "ngSanitize", "Directives", "DirectivesApiRestf
 	$scope.crudCategorie = 1;
 	$scope.crudCategorieMax = 0;
 		
-	console.info($location.search());
 	localStorage.setItem('token', $location.search().token);
 	$location.url($location.path());
 	//$location.path($scope.vueCourante);
