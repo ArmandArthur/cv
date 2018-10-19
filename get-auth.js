dd772a32952dea24acbdb239dae8329aaabda46b
ArmandArthur <armand.arthur@gmail.com>
2018-10-19 22:52:42 +0200

    new version

--- get-auth.js
+++ get-auth.js
@@ -5,18 +5,12 @@ module.exports = (req, res, next) => {
     try {
         console.info(req.headers);
         const token = req.headers.authorization.split(" ")[1];
-        if(token == undefined)
-        {
-            res.redirect(url.format({
-               pathname: "/preindex.html",
-             }));
-        }
         const decoded = jwt.verify(token, "ArthurMaelleProgrammation-3.0");
         req.email = decoded;
         next();
     } catch (error) {
-        return res.status(401).json({
-            message: error
-        });
+              return      res.redirect(url.format({
+               pathname: "/preindex.html",
+             }));
     }
 };
