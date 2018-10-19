2424165a6e1d2e4e7a94103dbaa52bcf425fa4f0
ArmandArthur <armand.arthur@gmail.com>
2018-10-19 19:59:44 +0200

    new version

--- get-auth.js
+++ get-auth.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
         next();
     } catch (error) {
         return res.status(401).json({
-            message: 'Authentification missed'
+            message: error
         });
     }
 };
