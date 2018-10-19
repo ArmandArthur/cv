43dea378119881a5bab00f9a70b7a4bf8d90eff8
ArmandArthur <armand.arthur@gmail.com>
2018-10-20 00:29:42 +0200

    new version

--- serveur.js
+++ serveur.js
@@ -17,7 +17,7 @@ app.use("/", serveStatic(__dirname+"/preindex.html"));
 
 
 app.use(bodyParser.json());
-app.use(cors({origin: 'http://armand-arthur.com:8085'}))
+app.use(cors({origin: 'http://armand-arthur.com'}))
 app.use(bodyParser.urlencoded({
   extended: true
 }));
