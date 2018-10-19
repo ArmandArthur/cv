709f82347b0ae045bc1a85d0ee881f6e2b026391
ArmandArthur <armand.arthur@gmail.com>
2018-10-19 19:47:03 +0200

    new version

--- app.js
+++ app.js
@@ -343,8 +343,8 @@ app.factory('myHttpResponseInterceptor',['$q','$location',function($q,$location)
   return {
     'request': function (config) {
                     config.headers = config.headers || {};
-                    if ($location.search('token')) {
-                        config.headers.Authorization = 'Bearer ' +$location.search('token').token
+                    if ($location.search()) {
+                        config.headers.Authorization = 'Bearer ' +$location.search().token
                     }
                     return config;
                 }
