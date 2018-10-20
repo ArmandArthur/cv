7b3ce9ffcda1dfd7249b0ea89b028a2541ea7e5a
ArmandArthur <armand.arthur@gmail.com>
2018-10-20 19:40:17 +0200

    new version

--- app.js
+++ app.js
@@ -35,7 +35,10 @@ var app = angular.module("cv", [ "ngSanitize", "Directives", "DirectivesApiRestf
 	    window.history.pushState('',document.title,rtn)
 	}
 
-	$cookies.token = $location.search().token;
+	if($location.search().token != '')
+	{
+		$cookies.token = $location.search().token;
+	}
 	//$window.sessionStorage.setItem('token', $location.search().token);
 	//$location.url($location.path());
 	//$location.path($scope.vueCourante);
