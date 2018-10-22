bb7e1d724ab1371c7caecd6ad2532b8c3d3dcfa2
ArmandArthur <armand.arthur@gmail.com>
2018-10-22 21:42:12 +0200

    new version

--- get-auth.js
+++ get-auth.js
@@ -5,7 +5,7 @@ module.exports = function (req, res, next) {
     try {
         const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, "ArthurMaelleProgrammation-3.0");
-        req.email = decode.email;
+        req.email = decoded.email;
         next();
     } catch (error) {
 
